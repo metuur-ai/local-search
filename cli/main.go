348f00988eaad98fb3b44c940cce485474afe79f
javierbenavides <javierhbr@gmail.com>
2026-07-24 15:14:54 -0500

    feat: update version to 0.3.12 across multiple files and adjust binary distributions

--- cli/main.go
+++ cli/main.go
@@ -25,7 +25,7 @@ import (
 
 // ── Config ────────────────────────────────────────────────────────────────────
 
-const Version = "0.3.11"
+const Version = "0.3.12"
 
 var (
 	appDir    = filepath.Join(homeDir(), ".local-search")
