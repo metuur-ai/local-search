3fa6444713be8a9a9955fe493204beefa5beb605
javierbenavides <javierhbr@gmail.com>
2026-07-24 13:55:32 -0500

    feat: update version to 0.3.9 and adjust package metadata across multiple files

--- cli/main.go
+++ cli/main.go
@@ -25,7 +25,7 @@ import (
 
 // ── Config ────────────────────────────────────────────────────────────────────
 
-const Version = "0.3.8"
+const Version = "0.3.9"
 
 var (
 	appDir    = filepath.Join(homeDir(), ".local-search")
