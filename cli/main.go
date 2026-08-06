87b941eae09f01c160e9795b42e0f6742d5a921f
javierbenavides <javierhbr@gmail.com>
2026-08-06 10:49:55 -0500

    chore: update version to 0.4.6 across all relevant files

--- cli/main.go
+++ cli/main.go
@@ -27,7 +27,7 @@ import (
 
 // ── Config ────────────────────────────────────────────────────────────────────
 
-const Version = "0.4.5"
+const Version = "0.4.6"
 
 var (
 	appDir    = filepath.Join(homeDir(), ".local-search")
